778df356268d684123d7e9087c63c8a1df8a7b87
Liza Carvelli <liza@carvel.li>
2023-09-26 19:58:51 +0200

    Change blacklist

--- ARDiscard/GameData/InternalConfiguration.cs
+++ ARDiscard/GameData/InternalConfiguration.cs
@@ -4,11 +4,17 @@ namespace ARDiscard.GameData;
 
 internal static class InternalConfiguration
 {
-    public static IReadOnlyList<uint> BlacklistedItems = new List<uint>
+    public static readonly IReadOnlyList<uint> BlacklistedItems = new List<uint>
     {
         2820, // red onion helm
         16039, // ala mhigan earrings
         24589, // aetheryte earrings
         33648, // menphina's earrings
+
+        21197, // UCOB
+        23175, // UWU
+        28633, // TEA
+        36810, // DSR
+        38951, // TOP
     }.AsReadOnly();
 }
